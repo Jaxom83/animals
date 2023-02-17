d546163a3439886060401798307aa8b37cd260e5
Jaxom83 <112565968+Jaxom83@users.noreply.github.com>
2023-02-17 20:59:56 +0100

    Kod 17-02-2023 21:00

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import "./App.css";
+
 import { useState } from "react";
 import AnimalShow from "./AnimalShow";
 
@@ -17,9 +19,9 @@ function App() {
     return <AnimalShow type={animal} key={index} />;
   });
   return (
-    <div>
+    <div className="app">
       <button onClick={handleClick}>Add Animal</button>
-      <div>{renderedAnimals}</div>
+      <div className="animal-list">{renderedAnimals}</div>
     </div>
   );
 }
